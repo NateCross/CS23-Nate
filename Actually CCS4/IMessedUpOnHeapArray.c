03004eb07dc1887a20a49d58b69422e4e8f7a945
Nathan Angelo Cruz <jonathan.a.b.cross@gmail.com>
2021-11-08 11:14:49 +0800

    Added heap reference

--- Actually CCS4/IMessedUpOnHeapArray.c
+++ Actually CCS4/IMessedUpOnHeapArray.c
@@ -1,9 +1,8 @@
-// TODO: Add heapify function
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <conio.h>
+#include <string.h>
 
 #define S 10
 
@@ -12,8 +11,15 @@ typedef struct {
     int size;
 } PRIORITY_QUEUE;
 
-bool isFull (PRIORITY_QUEUE heap) {
-    if (heap.size == S)
+bool isFull(PRIORITY_QUEUE heap) {
+    if (heap.size == S - 1)
+        return true;
+    else
+        return false;
+}
+
+bool isEmpty(PRIORITY_QUEUE heap) {
+    if (heap.size == 0)
         return true;
     else
         return false;
@@ -34,18 +40,63 @@ void insert(int input, PRIORITY_QUEUE *heap) {
         i = ++heap->size;    // So it starts at 1 and not 0, also auto increments
         while (heap->pQueue[i/2] > input) {
             heap->pQueue[i] = heap->pQueue[i/2];
-            i = i >> 1;     // Divide i by 2, but cheaper and sexier
+            i /= 2;     
         }
         heap->pQueue[i] = input;
     }
     return;
 }
 
+void swap(int *i, int *smaller) {
+    int temp = *i;
+    *i = *smaller;
+    *smaller = temp;
+    return;
+}
+
+void heapify(int i, PRIORITY_QUEUE *heap) {
+    int smaller, left = i * 2, right = i * 2 + 1;
+
+    // These are to check which one among i and its children is the smallest
+    if (left <= heap->size && heap->pQueue[left] < heap->pQueue[i])
+        smaller = left;
+    else
+        smaller = i;
+    if (right <= heap->size && heap->pQueue[right] < heap->pQueue[smaller])
+        smaller = right;
+
+    if (smaller != i) {
+        swap(&heap->pQueue[i], &heap->pQueue[smaller]);
+        heapify(smaller, heap);
+    }
+    return;
+}
+
+PRIORITY_QUEUE heapifyRecursive(int i, PRIORITY_QUEUE heap) {
+    int smaller, left = i * 2, right = i * 2 + 1;
+    
+    if (left <= heap.size && heap.pQueue[left] < heap.pQueue[i])
+        smaller = left;
+    else
+        smaller = i;
+    if (right <= heap.size && heap.pQueue[right] < heap.pQueue[smaller])
+        smaller = right;
+
+    if (smaller != i) {
+        swap(&heap.pQueue[i], &heap.pQueue[smaller]);
+        heap = heapifyRecursive(smaller, heap);
+    }
+
+    return heap;
+}
+
 int heapDeleteMin(PRIORITY_QUEUE *heap) {
     int min = heap->pQueue[1];
     heap->pQueue[1] = heap->pQueue[heap->size];
     heap->size--;
-    // heapify(1, *heap);
+    
+    // heapify(1, heap);
+    *heap = heapifyRecursive(1, *heap);
     
     return min;
 }
@@ -56,17 +107,45 @@ void printQueue(PRIORITY_QUEUE heap) {
     return;
 }
 
+void buildHeap(PRIORITY_QUEUE *heap) {
+    for (int i = heap->size / 1; i >= 1; i--) {
+        heapify(i, heap);
+    }
+    return;
+}
+
 int main() {
-    int test = 5;
-    PRIORITY_QUEUE lmao = initializeQueue();
-    insert(10, &lmao);
-    insert(5, &lmao);
-    insert(15, &lmao);
-    insert(1, &lmao);
-    insert(3, &lmao);
+    char           choice[100];
+    int            input;
+    PRIORITY_QUEUE lmao        = initializeQueue();
+    while (true) {
+        system("cls");
+        printQueue(lmao);
+        printf("\nCommands:\n");
+        printf("del - delete minimum\n");
+        printf("q - quit\n");
+        printf("build - build heap\n");
+        printf("any number - insert\n");
+        printf("\nEnter command: ");
 
-    printQueue(lmao);
+        // Proper way to do gets from now on; 
+        // without strcspn inputs aren't clean and program won't work
+        fgets(choice, 100, stdin);
+        choice[strcspn(choice, "\n")] = 0;
 
+        if (strcmp(choice, "del") == 0) 
+            heapDeleteMin(&lmao);
+        else if (strcmp(choice, "q") == 0)
+            break;
+        else if (strcmp(choice, "build") == 0)
+            buildHeap(&lmao);
+        else {
+            input = atoi(choice);
+            insert(input, &lmao);
+        }
+    }
+
+    printf("Press any key to end.\n");
     _getch();
     return 0;
 }
